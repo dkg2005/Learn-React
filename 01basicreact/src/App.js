d56af97f59084284864ff2ca8f1bce0673a3aca7
dkg2005 <dhruvgupta766671@gmail.com>
2024-07-20 00:37:13 +0530

    commited

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,9 +1,12 @@
-
+import New from './New'
 
 function App() {
   return (
+    <>
+    <New/>
     <h1>Hey, React Developer | [Dhruv Gupta]
     </h1>
+    </>
   );
 }
 
---
+++ 01basicreact/src/New.js
@@ -0,0 +1,7 @@
+function New () {
+    return(
+     <h2> Testing a new file in 01basicreact src</h2>
+    )
+}
+
+export default New
