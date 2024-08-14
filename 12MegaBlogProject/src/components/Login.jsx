d018dacf0fc41f6dc7cfb2c3eaafc8e4a7f7df62
dkg2005 <dhruvgupta766671@gmail.com>
2024-08-14 13:05:51 +0530

    commit

--- 12MegaBlogProject/src/components/Login.jsx
+++ 12MegaBlogProject/src/components/Login.jsx
@@ -52,8 +52,26 @@ function Login() {
                          label="Email: "
                          Placeholder="Enter Your email"
                          type="email"
-                         {...register("email")}
+                         {...register("email", {
+                          required :true,
+                          validate :{
+                            matchPatern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) ||
+                            "Email address must be a valid address",
+                          }
+                         })}
                      />
+                     <Input 
+                      label="Password: "
+                      type="password"
+                      Placeholder="Enter Your password"
+                      {...register("password",{
+                        required: true,
+                      })}
+                     />
+                     <Button
+                     type="submit"
+                     className="w-full"
+                     >Sign in</Button>
                     </div>
                  </form>   
 
---
+++ 12MegaBlogProject/src/components/Signup.jsx
@@ -0,0 +1,24 @@
+import React,{useState} from 'react'
+import authService from '../appwrite/auth'
+import { Link, useNavigate } from 'react-router-dom'
+import { login } from '../store/authSlice'
+import {Button ,Input, Logo} from './index'
+import { useDispatch } from 'react-redux'
+import { useForm } from 'react-hook-form'
+
+function Signup() {
+    const navigate = useNavigate();
+    const [error,setError] = useState("");
+    const dispatch = useDispatch();
+    const {register, handleSubmit} = useForm();
+    
+    const create = async
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Signup
