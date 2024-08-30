bf741b59bacb46f6950377f44819adb34bdea67c
dkg2005 <dhruvgupta766671@gmail.com>
2024-08-30 18:58:51 +0530

    All posts debugged , which are due to that correctly props are not passed in the postcard component which we are using invthe apppost.jsx file

--- 12MegaBlogProject/src/components/PostCard.jsx
+++ 12MegaBlogProject/src/components/PostCard.jsx
@@ -4,13 +4,12 @@ import {Link} from 'react-router-dom'
 
 
 function PostCard({$id, title, featuredImage}) {
- // console.log("featuredImage:", featuredImage);
 
   return (
    <Link to={`/post/${$id}`}> 
      <div className='w-full bg-gray-100 rounded-xl p-4'>
          <div className='w-full justify-center mb-4'>
-            <img src={appwriteService.getFilPreview(featuredImage)} alt={title} className='rounded-xl '/>
+         <img src={appwriteService.getFilPreview(featuredImage)} alt={title} className='rounded-xl '/>
          </div> 
          <h2
           className='text-xl fort-bold'
@@ -21,3 +20,24 @@ function PostCard({$id, title, featuredImage}) {
 }
 
 export default PostCard
+
+
+// FOR DEBUGGING THE ERROR IN GETTING THE ALL POSTS , ALSO MAKE CHANGES IN THE AllPosts.JSX file IN WHICH PROPS ARE NOT PASSED CORRECTLY IN THE PostCard
+// {imageUrl ? (
+//    <img src={imageUrl} alt={title} className='rounded-xl' />
+// ) : (
+//    <img src="https://via.placeholder.com/150" alt={title} className='rounded-xl' />
+// )}
+// let imageUrl = '';
+
+   //  if (featuredImage) {
+   //      try {
+   //          imageUrl = appwriteService.getFilPreview(featuredImage).toString();
+   //          console.log("Generated Image URL:", imageUrl);
+   //      } catch (error) {
+   //          console.error("Error generating image URL:", error);
+   //      }
+   //  } else {
+   //   //  console.warn("featuredImage is missing or undefined" ,$id);
+   //  // console.log($id)
+   //  }
--- 12MegaBlogProject/src/components/post-form/PostForm.jsx
+++ 12MegaBlogProject/src/components/post-form/PostForm.jsx
@@ -19,8 +19,8 @@ export default function PostForm({ post }) {
     const userData = useSelector((state) => state.auth.userData);
 
     const submit = async (data) => {
-        console.log("post :",post)
-        console.log("userData :",userData)
+       // console.log("post :",post)
+       // console.log("userData :",userData)
         
         if (post) {
             const file = data.image[0] ? await appwriteService.uploadFile(data.image[0]) : null;
